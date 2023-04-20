2a397f6fe5cce6ec26796ac840ae9e195f8a0433
Matt Nowicky <nowickymatt@outlook.com>
2023-04-20 12:58:11 -0400

    Create disableUAC.ps1
    Turns off User Account Control (UAC) prompts for all users.

---
+++ disableUAC.ps1
@@ -0,0 +1,16 @@
+$version = (Get-CimInstance Win32_OperatingSystem).Version
+$major = $version.split(".")[0]
+if ($major -eq 10) {
+	Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0"
+} ElseIf ($major -eq 6) {
+	$minor = $version.split(".")[1]
+    if ($minor -eq 1 -or $minor -eq 0) {
+		Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "0"
+    } Else {
+		Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0"
+    }
+} ElseIf ($major -eq 5) {
+	Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "0"
+} Else {
+	Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0"
+}
